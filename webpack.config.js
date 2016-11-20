7b840b076a2d2202d0f8dfb699a23a8dd55129f0
Deniz Gurkaynak <dgurkaynak@gmail.com>
2016-11-21 00:18:49 +0300

    chore(*): Add bare builds

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,26 @@ module.exports = [
         devtool: 'source-map'
     },
     {
+        name: 'commonjs:modern',
+        entry: './src/event-emitter-extra.js',
+        output: {
+            filename: 'commonjs.modern.js',
+            path: __dirname + '/dist',
+            libraryTarget: 'commonjs2'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    loaders: ['babel-loader']
+                },
+                { test: /promise-polyfill/, loader: 'ignore-loader' }
+            ]
+        },
+        devtool: 'source-map'
+    },
+    {
         name: 'globals',
         entry: './src/event-emitter-extra.js',
         output: {
@@ -36,6 +56,27 @@ module.exports = [
                     exclude: /(node_modules)/,
                     loaders: ['babel-loader']
                 }
+            ]
+        },
+        devtool: 'source-map'
+    },
+    {
+        name: 'globals:modern',
+        entry: './src/event-emitter-extra.js',
+        output: {
+            filename: 'globals.modern.js',
+            path: __dirname + '/dist',
+            library: 'EventEmitterExtra',
+            libraryTarget: 'var'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    loaders: ['babel-loader']
+                },
+                { test: /promise-polyfill/, loader: 'ignore-loader' }
             ]
         },
         devtool: 'source-map'
